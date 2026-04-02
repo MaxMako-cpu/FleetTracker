b3f81ad6978b5c800e03fdd51da21d33ae493478
MaxMako-cpu <m.kozhemiakin@gmail.com>
2026-04-02 13:49:10 +0000

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,8 @@
-const CACHE = 'fleettrack-v1';
-const ASSETS = ['/', '/index.html', '/manifest.json'];
-
-self.addEventListener('install', e => {
-  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
-  self.skipWaiting();
+// v2 - force cache clear
+const CACHE = ‘fleettrack-v2’;
+self.addEventListener(‘install’, e => { self.skipWaiting(); });
+self.addEventListener(‘activate’, e => {
+e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => caches.delete(k)))));
+self.clients.claim();
 });
-
-self.addEventListener('activate', e => {
-  e.waitUntil(caches.keys().then(keys =>
-    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
-  ));
-  self.clients.claim();
-});
-
-self.addEventListener('fetch', e => {
-  e.respondWith(
-    fetch(e.request).catch(() => caches.match(e.request))
-  );
-});
+self.addEventListener(‘fetch’, e => { e.respondWith(fetch(e.request)); });
